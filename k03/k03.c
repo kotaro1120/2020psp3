1b55e0a3cd747739fed539f7c225b9a10256cbb5
飛永虎汰郎 <bb35319028@ms.nagasaki.u-ac.jp>
2020-12-13 22:00:07 +0900

    k03

--- k03/k03.c
+++ k03/k03.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 #define DEBUG
 
 #define ALPHABET_LEN    255
@@ -10,15 +10,81 @@ char StrKey[] = "wind";
 char* ForceSearch(char text[], char key[])
 {
     //  ここを実装する
+    int start = 0, pos = 0, key_len, text_len;
 
+    text_len = strlen(text);
+    key_len = strlen(key);
+
+    for(start=0; start<text_len-key_len; start++)
+    {
+        for(pos=0; pos<key_len; pos++)
+    {
+        if(text[start+pos] != key[pos])
+        {
+            break;
+        }
+        if(pos==key_len-1)
+        {
+            return key;
+        }
+    }
+    
+    }
+return NULL;
 }
 
 char* BMSearch(char text[], char key[])
 {
     //  ここを実装する
+    int index, text_len = 0, key_len = 0, key_index, i, index_record, table[256];
 
+    while(text[text_len] != '\0')
+    {
+        text_len++;
+    }
+    
+    while(key[key_len] != '\0')
+    {
+        key_len++;
+    }
+
+    for(i=0; i<256; i++)
+    {
+        table[i] = key_len;
+    }
+    for(i=0; i<key_len; i++)
+    {
+        table[(int)key[i]] = key_len - 1;
+    }
+
+    index = key_len - 1;
+
+    while(index < text_len)
+    {
+        index_record = index;
+        for(key_index=key_len-1; key_index>=0; key_index--, index--)
+        {
+         if(text[index] == key[key_index])
+         {
+                if(key_index==0)
+            {
+                return text + index;
+            }
+         }else
+            {
+            break;
+            }   
+        }
+
+        index = index + table[(int)text[index]];
+
+        if(index<=index_record)
+        {   
+            index = index_record + 1;
+        }
+    }
+    return NULL;
 }
-
 int main(void)
 {
     char*   str;
